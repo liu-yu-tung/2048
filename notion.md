2c34bffc7d57a69d7d07e5aab70b5ef7536f89d4
YU TUNG LIU <yutungliu@liuyu.mshome.net>
2022-01-03 23:45:05 +0800

    Add new testing branch.

---
+++ notion.md
@@ -0,0 +1,1 @@
+![notion](https://regular-scar-cac.notion.site/2048-decfe1727d8b468db081ca4643485c6f)
---
+++ test/Board.h
@@ -0,0 +1,46 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include<iostream>
+#include<ctime>
+#include<cmath>
+using namespace std;
+
+enum Direction{
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        INVALID
+
+};
+
+class Board {
+    
+    public:
+
+    Board ();
+    int k;
+    bool flag;
+    bool change;
+    int i;
+    int j;
+    int ran1;
+    int ran2;
+    int val;
+    int reward;
+    
+    
+    int score;
+    char tiles[4][4];
+    void newTile();
+    int moveSingleLine(const char line[4], char new_line[4]);
+    //void displayBoard(const char tiles[4][4]) const;
+    //int moveBoard(char tiles[4][4], Direction dir);
+    bool checkGameOver(char tiles[4][4]) const;
+
+};
+   
+
+
+#endif
